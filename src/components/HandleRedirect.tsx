e7ba26cdfb10a7b35eded58c5f9e284ab4a242d7
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-05-01 22:30:04 +0100

    link redirection code

---
+++ src/components/HandleRedirect.tsx
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { Box, Spinner } from 'chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+export const SERVER_ENDPOINT =
+  process.env.REACT_APP_SERVER_ENDPOINT || 'http://localhost:3000';
+
+function HandleRedirect() {
+  const [destination, setDestination] = useState<null | string>(null);
+  const [error, setError] = useState();
+  const {
+    params: { shortId },
+  } = useRouteMatch<{
+    shortId: string;
+  }>();
+
+  useEffect(() => {
+    async function getData()) {
+      return axios
+        .get(`${SERVER_ENDPOINT}/api/url/${shortId}`)
+        .then((res) => 
+          setDestination(res.data.destination));
+       
+        .catch((err) => {
+          setError(err);
+        });
+    }
+    getData();
+  }, [shortId]);
+}
+
+export default HandleRedirect;
