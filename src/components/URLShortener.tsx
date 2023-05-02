572e811d94fc9cec0be8df81b21bfa32f2a9122a
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-05-02 21:17:50 +0100

    :chore: refactored for link creation

--- src/components/URLShortener.tsx
+++ src/components/URLShortener.tsx
@@ -1,44 +1,45 @@
 import axios from 'axios';
 import { useState } from 'react';
+
 import { Box, Button, Heading, Input, InputGroup } from '@chakra-ui/react';
+
 import { SERVER_ENDPOINT } from '../config/index';
-import ShortURL from '../../../server/src/models/shortURL.model';
 
 function URLShortener() {
-
-//when creatiing more complicated forms use a library like formik for state management instead
-  const [destination, setDestination] = useState()
-const [shortURL, setShortURL] = useState<{shortId: string} | null>(null)
+  // when creatiing more complicated forms use a library like formik for state management instead
+  const [destination, setDestination] = useState();
+  const [shortURL, setShortURL] = useState<{ shortId: string } | null>(null);
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-  setShortURL(null);
+    setShortURL(null);
 
-const result = await axios.post(`${SERVER_ENDPOINT}/api/createurl`, {
-      destination,
-    });
-    .then((resp) => 
-      resp.data);
-      console.log(result);
+    const result = await axios
+      .post(`${SERVER_ENDPOINT}/api/createurl`, {
+        destination,
+      })
+      .then((resp: { data: any }) => resp.data);
+    console.log(result);
 
+    // eslint-disable-next-line no-debugger
     debugger;
-  setShortURL(result);
+    setShortURL(result);
   }
   return (
     <Box pos="relative">
       <form onSubmit={handleSubmit}>
-     <InputGroup >
-      <Input
-      onChange={(e: any)=> setDestination(e.target.value)}
-      placeholder="https://example.com"
-       />
-      <Button type="submit">Shorten</Button>
-      </InputGroup >
+        <InputGroup>
+          <Input
+            onChange={(e: any) => setDestination(e.target.value)}
+            placeholder="https://example.com"
+          />
+          <Button type="submit">Shorten</Button>
+        </InputGroup>
       </form>
-      {shortURL &&(
-      <a href = {`${SERVER_ENDPOINT/shortURL?.shortId}`}>
-      Click Here!
-      </a>
+      {shortURL && (
+        <a href={`/${shortURL?.shortId}`}>
+          {window.location.origin}/${shortURL?.shortId}
+        </a>
       )}
     </Box>
   );
